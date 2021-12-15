ff66c7dc38f299b461cbc4bd31709632f9f27b85
任跃兵 <cnryb@live.cn>
2021-12-15 21:04:35 +0800

    disable https

--- Program.cs
+++ Program.cs
@@ -10,10 +10,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // app.UseHsts();ß
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
